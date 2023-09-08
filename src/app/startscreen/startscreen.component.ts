ece012b3e261dc8bdff2b6353aaa67c82a7e2098
Diana Asmus <asmus.diana@icloud.com>
2023-09-08 22:41:22 +0200

    M13V03 Spiel starten

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
   templateUrl: './startscreen.component.html',
   styleUrls: ['./startscreen.component.scss']
 })
-export class StartscreenComponent {
+export class StartscreenComponent implements OnInit{
 
+  constructor(private router: Router){ }
+
+  ngOnInit(): void {
+    
+  }
+
+  newGame() {
+    this.router.navigateByUrl('/game');
+  }
 }
